37a3b56179319adaa6ea1d0827070067cd50c393
Amir <4t5z7m7f8y@privaterelay.appleid.com>
2026-08-02 19:19:36 +0200

    Initial Commit

---
+++ Railmates/RailmatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailmatesApp.swift
+//  Railmates
+//
+//  Created by Amir Kozarcanin on 2026-08-02.
+//
+
+import SwiftUI
+
+@main
+struct RailmatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
